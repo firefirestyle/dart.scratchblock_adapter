93fc2fb1bb1a849ca497c6d40991092b2d5a4eeb
kyorohiro <kyorohiro@gmail.com>
2016-10-11 23:48:11 +0900

    add package fuction

--- lib/scratchblock.dart
+++ lib/scratchblock.dart
@@ -1,6 +1,14 @@
 import 'dart:js' as js;
 import 'dart:html' as html;
 
+String createSVG(String source, String language) {
+  return (new ScratchBlocks()).bakeSVG(source);
+}
+
+String translateSource(String source, String language) {
+  return (new ScratchBlocks()).translateSource(source, language);
+}
+
 class ScratchBlocks {
   static final String german = "ge";
   static final String spanish = "sp";
@@ -20,30 +28,29 @@ class ScratchBlocks {
   static final String indonesia = "id";
   static final String english = "en";
 
-
-  Map<String,List<String>> _language = {"languages":[]};
+  Map<String, List<String>> _language = {"languages": []};
   void addLang(String langage) {
     var langs = _language["languages"];
-    if(false ==  langs.contains(langage)) {
+    if (false == langs.contains(langage)) {
       langs.add(langage);
     }
   }
 
   void deleteLand(String langage) {
     var langs = _language["languages"];
-    if(true ==  langs.contains(langage)) {
+    if (true == langs.contains(langage)) {
       langs.remove(langage);
     }
   }
 
   String bakeSVG(String source) {
-    return js.context.callMethod("scratchblocks", [source,_language]);
+    return js.context.callMethod("scratchblocks", [source, _language]);
   }
 
-  String translateSource(String source, String language){
+  String translateSource(String source, String language) {
     js.JsObject sb2Obj2 = js.context["scratchblocks"];
-    js.JsObject s3 = sb2Obj2.callMethod("parse", [source,_language]); // "parse"];
-    s3.callMethod("translate",[js.context["scratchblocks"]["allLanguages"][language]]);
-    return sb2Obj2.callMethod("stringify",[s3]);
+    js.JsObject s3 = sb2Obj2.callMethod("parse", [source, _language]); // "parse"];
+    s3.callMethod("translate", [js.context["scratchblocks"]["allLanguages"][language]]);
+    return sb2Obj2.callMethod("stringify", [s3]);
   }
 }
